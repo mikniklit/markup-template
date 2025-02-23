7db521f269110b053a75919a2fa5773948bc97cc
Mikhail Litvinov <mikniklit@gmail.com>
2025-02-23 21:19:45 +0500

    add pugbem package

--- gulpfile.js/tasks/templates.js
+++ gulpfile.js/tasks/templates.js
@@ -3,13 +3,14 @@ import plumber from 'gulp-plumber';
 import notify from 'gulp-notify';
 import beautify from 'gulp-beautify';
 import pug from 'gulp-pug';
+import pugbem from 'gulp-pugbem';
 
 import { templates } from '../config.js';
 
 gulp.task('templates', () => {
     return gulp.src(templates.src)
         .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
-        .pipe(pug())
+        .pipe(pug({plugins: [pugbem]}))
         .pipe(beautify.html())
         .pipe(gulp.dest(templates.dist))
 });
