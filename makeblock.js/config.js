6f4c6e38fa2baa595331400757070456c13030fd
Mikhail Litvinov <mikniklit@gmail.com>
2025-02-26 00:58:54 +0500

    using @use instead @import in scss files

--- makeblock.js/config.js
+++ makeblock.js/config.js
@@ -1,7 +1,14 @@
 export const blocksDir = 'src/blocks/';
 export const fileSources = {
 	pug: `mixin {blockName}()\n\t.{blockName}&attributes(attributes)\n\t\t`,
-	scss: `.{blockName} {\n\tdisplay: block;\n}`
+	scss: `@use '../../styles/utils/mixins';
+@use '../../styles/utils/vars';
+@use '../../styles/utils/spritePng';
+@use '../../styles/utils/spriteSvg';
+
+.{blockName} {
+    display: block;
+}`
 };
 export const includePaths = {
     pug: 'src/templates/utils/blocks.pug',
